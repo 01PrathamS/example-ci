cd14f780cbce9ffe9b5a5f43518a390216c67b14
Pratham_Savaliya <pratham21btai35@karnavatiuniversity.edu.in>
2024-05-06 14:58:20 +0530

    first_commit

---
+++ functions.py
@@ -0,0 +1,11 @@
+def add(a, b): 
+    return a + b 
+
+def subtract(a, b): 
+    return a + b 
+
+def multiply(a, b): 
+    return a * b 
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    return multiply(subtract(fahrenheit, 32), 9 / 5)
---
+++ test_functions.py
@@ -0,0 +1,16 @@
+from functions import add, subtract, multiply
+from functions import convert_fahrenheit_to_celsius as f2c
+import pytest  
+
+def test_add(): 
+    assert add(2, 3) == 5 
+    assert add('space', 'ship') == 'spaceship' 
+
+# def test_subtract(): 
+#     assert subtract(2, -3) == -1 
+
+# def test_convert_fahrenheit_to_celcius(): 
+#     assert f2c(32) == 0 
+#     assert f2c(122) == pytest.approx(50) 
+#     with pytest.raises(AssertionError):
+#         f2c(-600)
