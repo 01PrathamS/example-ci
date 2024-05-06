14f95631e27d59adaf6a8ea78446ab3426792a86
Pratham_Savaliya <pratham21btai35@karnavatiuniversity.edu.in>
2024-05-06 15:04:06 +0530

    test_added_for_subtract

--- test_functions.py
+++ test_functions.py
@@ -6,8 +6,8 @@ def test_add():
     assert add(2, 3) == 5 
     assert add('space', 'ship') == 'spaceship' 
 
-# def test_subtract(): 
-#     assert subtract(2, -3) == -1 
+def test_subtract(): 
+    assert subtract(2, -3) == -1 
 
 # def test_convert_fahrenheit_to_celcius(): 
 #     assert f2c(32) == 0 
